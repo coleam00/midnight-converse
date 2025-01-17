bac6594aa405025ad7e94280f08cd4225f1fba4c
Cole Medin <cole@dynamous.ai>
2025-01-17 14:45:42 -0600

    Updating public key and project URL to be placeholders (these can be public)

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gewcfncoqvxnkrbpfonk.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdld2NmbmNvcXZ4bmtyYnBmb25rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYxODIzMTAsImV4cCI6MjA1MTc1ODMxMH0.EF4gWxkxRLiw6V6H4gXTPGXiGDF5OEgfJIHySm73cTU';
+const supabaseUrl = 'Your Supabase URL';
+const supabaseKey = 'Your Supabase Public Key';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
